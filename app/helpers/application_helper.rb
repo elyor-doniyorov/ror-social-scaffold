0dec423b446c322dc2ad3dcd41a1db5098e1a7a9
elyor doniyorov <doniyorovelyor@gmail.com>
2021-05-25 18:30:08 +0500

    Modify application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  # def menu_link_to(link_text, link_path)
-  #   class_name = current_page?(link_path) ? 'menu-item active' : 'menu-item'
+  def menu_link_to(link_text, link_path)
+    class_name = current_page?(link_path) ? 'menu-item active' : 'menu-item'
 
-  #   content_tag(:div, class: class_name) do
-  #     link_to link_text, link_path
-  #   end
-  # end
+    content_tag(:div, class: class_name) do
+      link_to link_text, link_path
+    end
+  end
 
   # def like_or_dislike_btn(post)
   #   like = Like.find_by(post: post, user: current_user)
