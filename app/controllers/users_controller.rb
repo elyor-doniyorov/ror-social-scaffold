d5a58c2a48a103666ce5e21141d7d45623e28515
elyor doniyorov <doniyorovelyor@gmail.com>
2021-05-25 18:26:13 +0500

    Add index method for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  # def index
-  #   @users = User.all
-  #   @friends = current_user.friends.filter { |friend| friend if friend != current_user }
-  #   @pending_requests = current_user.pending_friends
-  #   @friend_requests = current_user.friend_requests
-  # end
+  def index
+    @users = User.all
+    @friends = current_user.friends.filter { |friend| friend if friend != current_user }
+    @pending_requests = current_user.pending_friends
+    @friend_requests = current_user.friend_requests
+  end
 
   # def show
   #   @user = User.find(params[:id])
