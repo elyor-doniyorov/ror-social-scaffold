2b692c00eb0b6638759a9e40160a4bdc2f72fcfb
elyor doniyorov <doniyorovelyor@gmail.com>
2021-05-25 18:07:22 +0500

    Rename column

--- db/migrate/20210525100011_rename_column_friendship.rb
+++ db/migrate/20210525100011_rename_column_friendship.rb
@@ -1,5 +1,5 @@
 class RenameColumnFriendship < ActiveRecord::Migration[6.1]
   def change
-    # rename_column :friendships, :status, :confirmed
+    rename_column :friendships, :status, :confirmed
   end
 end
