575f620b85364071fb59e08a80fff58d3831377b
elyor doniyorov <doniyorovelyor@gmail.com>
2021-05-25 18:11:21 +0500

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'posts#index'
 
-  # devise_for :users
+  devise_for :users
   # resources :users, only: [:index, :show] do
   #   resources :friendships, only: [:create] 
   #   member do
