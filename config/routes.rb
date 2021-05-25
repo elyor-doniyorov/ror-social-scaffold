736e69b11db1a5dd3c04d6925ea0b206994f6224
elyor doniyorov <doniyorovelyor@gmail.com>
2021-05-25 18:12:39 +0500

    Modify user routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,17 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   devise_for :users
-  # resources :users, only: [:index, :show] do
-  #   resources :friendships, only: [:create] 
-  #   member do
-  #     get 'confirm'
-  #     get 'deny'
-  #   end
-  # end
-  # resources :posts, only: [:index, :create] do
-  #   resources :comments, only: [:create]
-  #   resources :likes, only: [:create, :destroy]
-  # end
+  resources :users, only: [:index, :show] do
+    resources :friendships, only: [:create] 
+    member do
+      get 'confirm'
+      get 'deny'
+    end
+  end
+  resources :posts, only: [:index, :create] do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create, :destroy]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
