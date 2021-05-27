95b015a1ce8b3e1b5eeb9bdddc39c059fed7fc25
elyor doniyorov <doniyorovelyor@gmail.com>
2021-05-27 14:51:36 +0500

    Fix rubocop errors

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,48 +1,48 @@
 require 'rails_helper'
 RSpec.describe User do
-    describe 'associations' do
-        it 'should have many posts' do
-            expect {should belong_to(:posts)}
-        end
+  describe 'associations' do
+    it 'should have many posts' do
+      expect { should belong_to(:posts) }
     end
-    describe 'associations' do
-        it 'should have many comments' do
-            expect {should belong_to(:comments)}
-        end
+  end
+  describe 'associations' do
+    it 'should have many comments' do
+      expect { should belong_to(:comments) }
     end
-    describe 'associations' do
-        it 'should have many likes' do
-            expect {should belong_to(:likes)}
-        end
+  end
+  describe 'associations' do
+    it 'should have many likes' do
+      expect { should belong_to(:likes) }
     end
-    describe 'associations' do
-        it 'should have many confirmed_friendships' do
-            expect {should belong_to(:confirmed_friendships)}
-        end
+  end
+  describe 'associations' do
+    it 'should have many confirmed_friendships' do
+      expect { should belong_to(:confirmed_friendships) }
     end
-    describe 'associations' do
-        it 'should have many friends' do
-            expect {should belong_to(:friends)}
-        end
+  end
+  describe 'associations' do
+    it 'should have many friends' do
+      expect { should belong_to(:friends) }
     end
-    describe 'associations' do
-        it 'should have many pending_friendships' do
-            expect {should belong_to(:pending_friendships)}
-        end
+  end
+  describe 'associations' do
+    it 'should have many pending_friendships' do
+      expect { should belong_to(:pending_friendships) }
     end
-    describe 'associations' do
-        it 'should have many pending_friends' do
-            expect {should belong_to(:pending_friends)}
-        end
+  end
+  describe 'associations' do
+    it 'should have many pending_friends' do
+      expect { should belong_to(:pending_friends) }
     end
-    describe 'associations' do
-        it 'should have many inverse_friendships' do
-            expect {should belong_to(:inverse_friendships)}
-        end
+  end
+  describe 'associations' do
+    it 'should have many inverse_friendships' do
+      expect { should belong_to(:inverse_friendships) }
     end
-    describe 'associations' do
-        it 'should have many friend_requests' do
-            expect {should belong_to(:friend_requests)}
-        end
+  end
+  describe 'associations' do
+    it 'should have many friend_requests' do
+      expect { should belong_to(:friend_requests) }
     end
+  end
 end
