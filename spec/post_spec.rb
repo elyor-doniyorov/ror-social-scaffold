8193be78028d47051fa24b788011da7941ae572b
elyor doniyorov <doniyorovelyor@gmail.com>
2021-05-27 14:14:09 +0500

    Add tests post validations

---
+++ spec/post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+RSpec.describe Post do
+  describe 'associations' do
+    it 'should belongs to user' do
+      expect {should belong_to(:user)}
+    end
+  end
+  describe 'associations' do
+    it 'should have many comments' do
+      expect {should have_many(:comments)}
+    end
+  end
+  describe 'associations' do
+    it 'should have many likes' do
+      expect {should have_many(:likes)}
+    end
+  end
+end
