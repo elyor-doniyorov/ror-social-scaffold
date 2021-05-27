efbfc00aa30ee3d9fe9a138fd681a9cf582b7c44
elyor doniyorov <doniyorovelyor@gmail.com>
2021-05-27 14:16:26 +0500

    Fix rubocop errors

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 RSpec.describe Post do
   describe 'associations' do
     it 'should belongs to user' do
-      expect {should belong_to(:user)}
+      expect { should belong_to(:user) }
     end
   end
   describe 'associations' do
     it 'should have many comments' do
-      expect {should have_many(:comments)}
+      expect { should have_many(:comments) }
     end
   end
   describe 'associations' do
     it 'should have many likes' do
-      expect {should have_many(:likes)}
+      expect { should have_many(:likes) }
     end
   end
 end
