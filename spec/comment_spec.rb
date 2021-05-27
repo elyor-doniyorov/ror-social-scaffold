938e6c0f503ddd17b9f39f87fdb51c1316bb1883
elyor doniyorov <doniyorovelyor@gmail.com>
2021-05-27 14:31:12 +0500

    Add tests for comment validations

---
+++ spec/comment_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+RSpec.describe Comment do
+    describe 'associations' do
+        it 'should belongs to user' do
+            expect {should belong_to(:user)}
+        end
+    end
+    describe 'associations' do
+        it 'should belongs to post' do
+            expect {should belong_to(:post)}
+        end
+    end
+end
